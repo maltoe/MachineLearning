60bc57a65af38d35a3d418fe2116a588c47ef3d1
Malte Rohde <malte.rohde@gmx.de>
2012-04-22 19:15:16 +0200

    ueb2: Playing around with MT/array.h

--- ueb02/src/ueb2.cpp
+++ ueb02/src/ueb2.cpp
@@ -1,5 +1,23 @@
+#include <MT/array.h>
+
+#include <iostream>
+
 int main(int, char**)
 {
+    // Watch out: Different short-hands.
+    // ARR creates double vector with given elements
+    // arr creates matrix with given n,m dimensions
+    
+    // scalar product
+    MT::Array<double> a = ARR(1.0, 2.0);     
+    MT::Array<double> b = ARR(3.0, 4.0);
+    std::cout << (~a * b) << std::endl;
+    
+    // random 2x3 matrix
+    MT::Array<double> c = arr(2, 3);
+    rndInteger(c, -5, 5, false);
+    std::cout << c << std::endl;
+    
     return 0;
 }
 
